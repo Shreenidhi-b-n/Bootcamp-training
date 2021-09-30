392fc26f62b7c345ac1f15ce9e0c48f7c1326839
SHREENIDHI BN <86700449+Shreenidhi-b-n@users.noreply.github.com>
2021-09-30 20:30:09 +0530

    Add files via upload

---
+++ Java/CollectionBookStoreDemo/src/main/java/bookop/BookOperations.java
@@ -0,0 +1,14 @@
+package bookop;
+
+import model.Book;
+
+import java.util.ArrayList;
+
+public interface BookOperations {
+
+      String addABook(Book book);
+      ArrayList<Book> getAllBooks();
+      Book getABook(int bookId);
+      String setBookPrice(int bookId,int upPrice);
+
+}
